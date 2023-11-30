c825cf79bffa127a1e0cddf2b895a79c46392c0b
Gabriel <gabrieltocu@unisabana.edu.co>
2023-11-29 21:26:25 -0500

    Components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Header from './components/Header'
+import Form from './components/Form'
+import PatientsList from './components/PatientsList'
+
 
 function App() {
 
   return (
     <>
-      <h1>React 18 Alpha</h1>
-      <p>React 18 Alpha is here!</p>
+      <Header/>
+      <Form/>
+      <PatientsList/>
     </>
   )
 }
